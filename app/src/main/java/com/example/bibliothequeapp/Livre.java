be12cd47e35968bfa4e3a849909ac2a6c2a74c17
TonNom <oumargaye@esp.sn>
2026-05-23 14:35:03 +0000

    lab4 dev mobile

---
+++ app/src/main/java/com/example/bibliothequeapp/Livre.java
@@ -0,0 +1,32 @@
+package com.example.bibliothequeapp;
+public class Livre {
+
+    private int id;
+    private String titre;
+    private String auteur;
+    private String isbn;
+    private boolean disponible;
+
+    public Livre(int id, String titre, String auteur, String isbn, boolean disponible) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.isbn = isbn;
+        this.disponible = disponible;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitre() { return titre; }
+    public void setTitre(String titre) { this.titre = titre; }
+
+    public String getAuteur() { return auteur; }
+    public void setAuteur(String auteur) { this.auteur = auteur; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public boolean isDisponible() { return disponible; }
+    public void setDisponible(boolean disponible) { this.disponible = disponible; }
+}
---
+++ app/src/main/java/com/example/bibliothequeapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.bibliothequeapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewLivres;
+    private LivreAdapter livreAdapter;
+    private ArrayList<Livre> listeLivres;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerViewLivres = findViewById(R.id.recyclerViewLivres);
+
+        listeLivres = new ArrayList<>();
+
+        listeLivres.add(new Livre(1, "Le Petit Prince", "Antoine de Saint-Exupéry", "9780156013987", true));
+        listeLivres.add(new Livre(2, "L'Étranger", "Albert Camus", "9782070360024", false));
+        listeLivres.add(new Livre(3, "Les Misérables", "Victor Hugo", "9782253096344", true));
+        listeLivres.add(new Livre(4, "Une si longue lettre", "Mariama Bâ", "9782841290529", true));
+        listeLivres.add(new Livre(5, "Le Vieux Nègre et la Médaille", "Ferdinand Oyono", "9782264018304", false));
+        listeLivres.add(new Livre(6, "Madame Bovary", "Gustave Flaubert", "9782070409228", true));
+        listeLivres.add(new Livre(7, "La Peste", "Albert Camus", "9782070360420", false));
+        listeLivres.add(new Livre(8, "Sous l'orage", "Seydou Badian", "9782708707691", true));
+
+        recyclerViewLivres.setLayoutManager(new LinearLayoutManager(this));
+        livreAdapter = new LivreAdapter(listeLivres);
+        recyclerViewLivres.setAdapter(livreAdapter);
+    }
+}
